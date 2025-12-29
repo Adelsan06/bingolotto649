726ce0fbbb684698687a5025b491fb445d522e36
Adelsan06 <adel.g0820@gmail.com>
2025-12-29 13:26:21 -0500

    Add login and payment functions in app.js

---
+++ public/app.js
@@ -0,0 +1,20 @@
+async function login() {
+  const res = await fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value
+    })
+  });
+
+  const data = await res.json();
+  if (data.error) alert(data.error);
+  else window.location = "/play.html";
+}
+
+async function pay() {
+  const res = await fetch("/checkout", { method: "POST" });
+  const data = await res.json();
+  window.location = data.url;
+}
