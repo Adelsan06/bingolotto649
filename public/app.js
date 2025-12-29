b05deed311d142587d983bdbff1fe997469d07a7
Adelsan06 <adel.g0820@gmail.com>
2025-12-29 14:44:08 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,25 @@
 console.log("app.js loaded");
+async function signup() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  });
+
+  const data = await res.json();
+
+  if (data.error) {
+    alert(data.error);
+    return;
+  }
+
+  alert("Signup successful. You can now log in.");
+}
+
+
 async function login() {
   const res = await fetch("/login", {
     method: "POST",
